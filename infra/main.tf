e8618158098aaf6a847816b4daad03f65e442823
thales <thalesquadros2@gmail.com>
2024-10-13 13:33:42 -0300

    load balancer configuration with terraform

--- infra/main.tf
+++ infra/main.tf
@@ -31,12 +31,47 @@ resource "aws_key_pair" "ssh-key" {
 }
 
 resource "aws_autoscaling_group" "autoscaling_group" {
-  availability_zones = [ "${var.aws_region}a" ]
+  availability_zones = [ "${var.aws_region}a","${var.aws_region}b" ]
   name = var.groupName
   max_size = var.maxSize
   min_size = var.minSize
   launch_template {
     id = aws_launch_template.machine.id
     version = "$Latest"
+  }
+  target_group_arns = [ aws_lb_target_group.lb_target_group.arn ]
+}
+
+resource "aws_default_subnet" "first_subnet" {
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_default_subnet" "second_subnet" {
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets = [ aws_default_subnet.first_subnet.id, aws_default_subnet.second_subnet.id ]
+}
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name = "targetMachines"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "input_loadbalancer" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
   }
 }
